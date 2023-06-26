0bef4183141bc8434000056a8a60b634a2446aa3
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-06-26 21:31:17 +0200

    added functionaliy to the singleComponent

--- src/components/SingleColor.jsx
+++ src/components/SingleColor.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import {toast} from "react-toastify";
+import React from "react";
+import { toast } from "react-toastify";
 
-const SingleColor = ({index, color}) => {
-    const {hex, weight} = color;
-    
+const SingleColor = ({ index, color }) => {
+  const { hex, weight } = color;
+  const saveToClipboard = async () => {
+    // navigator is a specil key word that comes from the brower
+  };
+
   return (
-    <div>SingleColor</div>
-  )
-}
+    <article
+      className={index > 10 ? "color color-light" : "color"}
+      style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hex}%</p>
+    </article>
+  );
+};
 
-export default SingleColor
+export default SingleColor;
